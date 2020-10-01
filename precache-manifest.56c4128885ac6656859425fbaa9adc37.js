26bc59aafbe86c7cb9b747aaf9afdde9ecdc250e
Heeyeon <yyyy1221@naver.com>
2020-10-01 22:06:02 +0900

    Updates

---
+++ precache-manifest.56c4128885ac6656859425fbaa9adc37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f28c78daf85aca644272a88f7b2ab57",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "974a567962352833235a",
+    "url": "/react_test/static/css/main.ebf7e114.chunk.css"
+  },
+  {
+    "revision": "8cd6e783f05307483fae",
+    "url": "/react_test/static/js/2.affe8b72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_test/static/js/2.affe8b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "974a567962352833235a",
+    "url": "/react_test/static/js/main.7029a00a.chunk.js"
+  },
+  {
+    "revision": "dfab607d5f73d6480f5b",
+    "url": "/react_test/static/js/runtime-main.961c181a.js"
+  }
+]);
